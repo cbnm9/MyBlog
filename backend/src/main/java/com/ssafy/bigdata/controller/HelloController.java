fb4087ea4068abf333220e8d6b57c8d3bc6b6fe7
Minseong Seo <cbnm943@gmail.com>
2020-08-27 09:40:08 +0900

    Initial Commit

---
+++ backend/src/main/java/com/ssafy/bigdata/controller/HelloController.java
@@ -0,0 +1,35 @@
+package com.ssafy.bigdata.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ssafy.bigdata.model.RestResponse;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+
+@CrossOrigin(origins = { "*" })
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    
+    @ApiOperation(value = "테스트에여")
+    @GetMapping("/hello")
+    public Object helloWorld() {
+        final RestResponse response = new RestResponse();
+        
+        List<String> list = new ArrayList<String>();
+        list.add("안녕");
+        list.add("하세오");
+
+        response.status = true;
+        response.msg = "안녕하세요!";
+        response.data = list;
+
+        return response;
+    }
+}
---
+++ backend/src/main/java/com/ssafy/bigdata/model/RestResponse.java
@@ -0,0 +1,7 @@
+package com.ssafy.bigdata.model;
+
+public class RestResponse {
+    public boolean status;
+    public String msg;
+    public Object data;
+}
